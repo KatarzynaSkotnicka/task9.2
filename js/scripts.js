38f78b7eebb6e8544be4599e1e70ae34bcc06323
Katarzyna Skotnicka <ka.skotnicka@gmail.com>
2018-01-11 10:32:30 +0100

    Fix js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,10 @@
-var female = ["Asia", "Halina", "Agnieszka", "Alicja", "Magda"];
-
-	male = ["Robert","Paweł", "Artur", "Rafał", "Olek"];
-
-
-	both = female.concat(male);
-
-
+var female = ["Asia", "Halina", "Agnieszka", "Alicja", "Magda"],
+	male = ["Robert","Paweł", "Artur", "Rafał", "Olek"],
+	both = female.concat(male),
 	newName = "Zenek";
 
-
 	if(both.indexOf(newName) === -1) {
-
 		both.push(newName);
 	}
-
 
 	console.log(both);
